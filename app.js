a3cc364199f78f1d230cf80143cc71ed3cd60bb7
Danny Jang <teru@Dannys-MacBook-Pro.local>
2019-03-31 22:33:22 -0700

    Fixed missing credentials error: attempt 5

--- app.js
+++ app.js
@@ -6,25 +6,14 @@ const admin = require("firebase-admin");
 
 const app = express();
 
-const serviceAccount = {
-  "type": process.env.type,
-  "project_id": process.env.project_id,
-  "private_key_id": process.env.private_key_id,
-  "private_key": process.env.private_key,
-  "client_email": process.env.client_email,
-  "client_id": process.env.client_id,
-  "auth_uri": process.env.auth_uri,
-  "token_uri": process.env.token_uri,
-  "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
-  "client_x509_cert_url": process.env.client_x509_cert_url
-}
-
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
 admin.initializeApp({
-  credential: admin.credential.cert(JSON.stringify(serviceAccount)),
+  credential: admin.credential.cert(
+    "private_key": process.env.private_key,
+    "client_email": process.env.client_email),
   databaseURL: 'https://dutchpay-109be.firebaseio.com'
 });
 
