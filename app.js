456f116d8cba46364cf78852bf6995e0b44088c9
Danny Jang <teru@Dannys-MacBook-Pro.local>
2019-04-01 01:20:50 -0700

    Test request 6

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.post("/", function(req, res) {
 });
 
 app.post("/registeremail", function(req, res) {
-  // console.log(req.body);
+  console.log(req.body);
   admin.auth().createUser({
       email: req.body.email,
       password: req.body.password,
